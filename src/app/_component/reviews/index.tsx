9edea8e4e4956bfe0ecbc5f8dc2602830e89b1a3
Ramon <ramonrodsouza@gmail.com>
2025-08-23 19:41:43 -0300

    feature-007 - criando componente review

---
+++ src/app/_component/reviews/index.tsx
@@ -0,0 +1,19 @@
+import { Box, Typography } from "@mui/material";
+import CardReview from "../cardReview";
+import { listReviews } from "./listReviews";
+
+export default function Reviews() {
+
+    return (
+        <Box component="section" className="section">
+            <Typography variant="h6" className="title">
+                <span className="bold">Depoimento</span> dos clientes<span className="bold">!</span>
+            </Typography>
+            <Box component="div" className="list">
+                {listReviews.map((it, index) => (
+                    <CardReview key={index} img={it.img} depoiment={it.depoiment} name={it.name} />
+                ))}
+            </Box>
+        </Box>
+    )
+}
