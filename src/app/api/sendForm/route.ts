9ab738a6d2d3997cbcddfe834106ea695786e46e
Ramon <ramonrodsouza@gmail.com>
2025-09-03 13:55:09 -0300

    feature-018 - feat: criando fetch para enviar form para webhook

---
+++ src/app/api/sendForm/route.ts
@@ -0,0 +1,23 @@
+import { WEBHOOK } from '@utils/string';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(req: NextRequest) {
+    const payload = await req.json();
+
+    if (!WEBHOOK) {
+        return NextResponse.json({ error: 'WEBHOOK não definido' }, { status: 500 });
+    }
+
+    try {
+        const response = await fetch(WEBHOOK, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(payload),
+        });
+
+        const data = await response.text();
+        return NextResponse.json({ status: 'ok', webhookResponse: data });
+    } catch (err) {
+        return NextResponse.json({ error: 'Erro ao enviar para o webhook', details: err }, { status: 500 });
+    }
+}
